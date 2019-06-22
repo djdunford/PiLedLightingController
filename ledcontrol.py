faada730f85f8b2f55af51fc928732a5a256701b
ddunford <djdunford@gmail.com>
2019-06-22 20:12:56 +0100

    fixed tabs (again)

--- ledcontrol.py
+++ ledcontrol.py
@@ -20,9 +20,9 @@ import ledstriplib
 
 # define status post function 
 def statusPost(status): 
-	syslog.syslog(status) 
-	return 
-	 
+    syslog.syslog(status) 
+    return 
+     
 # set global trigger to False - a callback routine can set this to a value to trigger a particular sequence 
 btnTrigger = 0 
 
@@ -42,27 +42,27 @@ buttons = [[3,1],[22,2],[9,3]]
 # define button press callback function
 def button_press_callback(channel):
 
-	# access global variables
-	global btnTrigger
-	global buttons
-	
-	# add 0.1s debounce 
-	time.sleep(0.1) 
+    # access global variables
+    global btnTrigger
+    global buttons
+    
+    # add 0.1s debounce 
+    time.sleep(0.1) 
 
-	# set sequence based on button pressed
-	for button in buttons:	
-		if channel == button[0]:	
-			if GPIO.input(button[0]) == False:
-				btnTrigger = button[1]			
-				
-	return
+    # set sequence based on button pressed
+    for button in buttons:    
+        if channel == button[0]:    
+            if GPIO.input(button[0]) == False:
+                btnTrigger = button[1]            
+                
+    return
 
 # set each button as an input pin, with pullup,
 # and set an event handler based on falling edge
 # detection with debounce time
 for button in buttons:
-	GPIO.setup(button[0], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.add_event_detect(button[0], GPIO.FALLING, callback=button_press_callback, bouncetime=50)
+    GPIO.setup(button[0], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(button[0], GPIO.FALLING, callback=button_press_callback, bouncetime=50)
 
 # Set default brightness for Blinkt! LEDs 
 ledstriplib.set_brightness(0.1) 
@@ -77,41 +77,41 @@ statusPost("RUNNING")
 # loop forever 
 while True: 
  
-	# if button 1 pressed show Red 
-	if btnTrigger == 1:
-		if stsSequence == 1:
-			statusPost('Sequence 1 cleared')
-			ledstrip.clear()
-		else:
-			statusPost('Sequence 1 triggered') 
-			ledstriplib.set_all(255,0,0)
+    # if button 1 pressed show Red 
+    if btnTrigger == 1:
+        if stsSequence == 1:
+            statusPost('Sequence 1 cleared')
+            ledstrip.clear()
+        else:
+            statusPost('Sequence 1 triggered') 
+            ledstriplib.set_all(255,0,0)
 
-		ledstriplib.show()
-		btnTrigger = 0 
-		stsSequence = 1 - stsSequence
+        ledstriplib.show()
+        btnTrigger = 0 
+        stsSequence = 1 - stsSequence
 
-	# if button 2 pressed show Red 
-	if btnTrigger == 2:
-		if stsSequence == 2:
-			statusPost('Sequence 2 cleared')
-			ledstrip.clear()
-		else:
-			statusPost('Sequence 2 triggered') 
-			ledstriplib.set_all(0,255,0)
+    # if button 2 pressed show Red 
+    if btnTrigger == 2:
+        if stsSequence == 2:
+            statusPost('Sequence 2 cleared')
+            ledstrip.clear()
+        else:
+            statusPost('Sequence 2 triggered') 
+            ledstriplib.set_all(0,255,0)
 
-		ledstriplib.show() 
-		btnTrigger = 0 
-		stsSequence = 2 - stsSequence
+        ledstriplib.show() 
+        btnTrigger = 0 
+        stsSequence = 2 - stsSequence
 
-	# if button 3 pressed show Red		  
-	if btnTrigger == 3:
-		if stsSequence == 3:
-			statusPost('Sequence 3 cleared')
-			ledstrip.clear()
-		else:
-			statusPost('Sequence 3 triggered') 
-			ledstriplib.set_all(0,0,255)
+    # if button 3 pressed show Red          
+    if btnTrigger == 3:
+        if stsSequence == 3:
+            statusPost('Sequence 3 cleared')
+            ledstrip.clear()
+        else:
+            statusPost('Sequence 3 triggered') 
+            ledstriplib.set_all(0,0,255)
 
-		ledstriplib.show(); 
-		btnTrigger = 0 
-  		stsSequence = 3 - stsSequence
+        ledstriplib.show(); 
+        btnTrigger = 0 
+          stsSequence = 3 - stsSequence
