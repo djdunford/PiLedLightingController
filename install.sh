1345d49b9c3809df237d1e3f771d4db99defc2b6
djdunford <45966937+djdunford@users.noreply.github.com>
2019-06-22 20:49:34 +0100

    First working version installed (#2)
    * Basic red/green/blue function on button 1/2/3
    
    * add executable flag
    
    * converted CRLF to LF
    
    * removed spurious space
    
    * fixed spacing
    
    * convert to tabs (again!)
    
    * now uses own copy of blinkt.py library
    
    blinkt.py modified to remove timing delays in the code
    
    * added import sys
    
    * added modules to install to install.sh
    
    * first working installed version
    
    * added toggle on/off function to each sequence
    
    * fixed tabs (again)
    
    * fixed tabs (yet again)
    
    * ledstriplib.clear()
    
    * corrected turn off logic
    
    * removed bug from blue sequence

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+# 
+# install.sh 
+# 
+# install PiLedLightingController as a service 
+# 
+# by Darren Dunford 
+# 
+ 
+ 
+# script control variables 
+servicedir="/opt/ledcontrol" 
+ 
+# install required modules
+apt-get install -y python3-pip rpi.gpio
+ 
+# create directory structure for apps 
+mkdir -p $servicedir/bin 
+mkdir -p $servicedir/etc 
+chown -R pi:pi $servicedir 
+ 
+# copy app and service files 
+cp ledcontrol.py $servicedir/bin 
+cp ledcontrol.service /lib/systemd/system 
+cp ledstriplib.py $servicedir/bin
+chmod 644 /lib/systemd/system/ledcontrol.service 
+ 
+# restart systemd and enable service on boot 
+systemctl daemon-reload 
+systemctl enable ledcontrol.service 
+systemctl start ledcontrol.service
---
+++ ledcontrol.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3 
+# 
+# Raspberry Pi LED Light Controller
+# for APA102C and SK9822 LED strips
+#
+# by Darren Dunford 
+ 
+# Import Libraries 
+import os 
+import RPi.GPIO as GPIO 
+import syslog 
+import sys
+import time
+
+# change current working directory
+os.chdir(os.path.dirname(sys.argv[0]))
+
+# import library from CWD
+import ledstriplib
+
+# define status post function 
+def statusPost(status): 
+    syslog.syslog(status) 
+    return 
+     
+# set global trigger to False - a callback routine can set this to a value to trigger a particular sequence 
+btnTrigger = 0 
+
+# set current sequence to false
+stsSequence = 0
+ 
+# Set the GPIO PIN naming mode
+GPIO.setmode(GPIO.BCM)
+
+# Suppress warnings for GPIO usage clashes
+GPIO.setwarnings(False)
+
+# create array for BCM lines corresponding to buttons
+# format is [ BCM line , button_number ]
+buttons = [[3,1],[22,2],[9,3]]
+
+# define button press callback function
+def button_press_callback(channel):
+
+    # access global variables
+    global btnTrigger
+    global buttons
+    
+    # add 0.1s debounce 
+    time.sleep(0.1) 
+
+    # set sequence based on button pressed
+    for button in buttons:    
+        if channel == button[0]:    
+            if GPIO.input(button[0]) == False:
+                btnTrigger = button[1]            
+                
+    return
+
+# set each button as an input pin, with pullup,
+# and set an event handler based on falling edge
+# detection with debounce time
+for button in buttons:
+    GPIO.setup(button[0], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(button[0], GPIO.FALLING, callback=button_press_callback, bouncetime=50)
+
+# Set default brightness for Blinkt! LEDs 
+ledstriplib.set_brightness(0.1) 
+ 
+# clear LEDs on startup
+ledstriplib.clear()
+ledstriplib.show()
+
+# post status message 
+statusPost("RUNNING") 
+
+# loop forever 
+while True: 
+ 
+    # if button 1 pressed show Red 
+    if btnTrigger == 1:
+        if stsSequence == 1:
+            statusPost('Sequence 1 cleared')
+            ledstriplib.clear()
+            stsSequence = 0
+        else:
+            statusPost('Sequence 1 triggered') 
+            ledstriplib.set_all(255,0,0)
+            stsSequence = 1
+
+        ledstriplib.show()
+        btnTrigger = 0 
+
+    # if button 2 pressed show Green
+    if btnTrigger == 2:
+        if stsSequence == 2:
+            statusPost('Sequence 2 cleared')
+            ledstriplib.clear()
+            stsSequence = 0
+        else:
+            statusPost('Sequence 2 triggered') 
+            ledstriplib.set_all(0,255,0)
+            stsSequence = 2
+
+        ledstriplib.show() 
+        btnTrigger = 0 
+
+    # if button 3 pressed show Blue          
+    if btnTrigger == 3:
+        if stsSequence == 3:
+            statusPost('Sequence 3 cleared')
+            ledstriplib.clear()
+            stsSequence = 0
+        else:
+            statusPost('Sequence 3 triggered') 
+            ledstriplib.set_all(0,0,255)
+            stsSequence = 3
+
+        ledstriplib.show(); 
+        btnTrigger = 0 
