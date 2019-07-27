cdec5087fe52c175a2147fdfd67d647bc15d10b0
Darren Ipad <djdunford@gmail.com>
2019-07-27 12:10:16 +0100

    refactored logging

--- api.py
+++ api.py
@@ -3,31 +3,23 @@ import boto3
 import logging
 
 logger = logging.getLogger("api")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 iotClient = boto3.client('iot-data', region_name='eu-west-1')
 
-def showsequence(event, context):
-    
-    logger.info("Received event: " + json.dumps(event, indent=2))
-    logger.info("sequence: " + event['pathParameters']['sequence'])
-    
-    #response = client.update_thing_shadow(thingName='ThomasBedroomLEDcontrol')
-    
-    
-    
-    #streamingBody = response["payload"]
-    #jsonState = json.loads(streamingBody.read())
-    #logger.info(jsonState)
- 
-    #return {'statusCode': 200, 'body': json.dumps(jsonState), 'headers': {'Content-Type': 'application/json'}}
-    return {'statusCode': 200}
+def showsequence(event, context):    
+    logger.debug("Received event: " + json.dumps(event, indent=2))
+    logger.info("Executing showsequence command: " + event['pathParameters']['sequence'])
+    response = client.update_thing_shadow(thingName='ThomasBedroomLEDcontrol')
+    streamingBody = response["payload"]
+    jsonState = json.loads(streamingBody.read())
+    logger.info(jsonState)
+    return {'statusCode': 200, 'body': json.dumps(jsonState), 'headers': {'Content-Type': 'application/json'}}
 
 def state(event, context):
+	logger.info("Executing state command (returns state)")
 	response = iotClient.get_thing_shadow(thingName='ThomasBedroomLEDcontrol')
 	streamingBody = response["payload"]
 	jsonState = json.loads(streamingBody.read())
-	logger.info(jsonState)
- 
+	logger.debug(jsonState)
 	return {'statusCode': 200, 'body': json.dumps(jsonState), 'headers': {'Content-Type': 'application/json'}}
 	
-
