ccd6c25f109a1d2365e6a6af3f3bbf660467b006
Darren Dunford <45966937+djdunford@users.noreply.github.com>
2019-07-28 08:05:10 +0100

    Feature/lambda api (#13)
    * added python runtime version to buildspec
    
    * changed AWS SAM header
    
    * added SAM installation to CodeBuild
    
    * updated buildspec file to list directory contents
    
    * updated codebuild buildspec file
    
    * added cat command to show file contents
    
    * syntax error in Serverless::Function
    
    * moved lambda app in to folder
    
    * removed sam build command
    
    * typo
    
    * flattened directory structure again
    
    * removed cd command
    
    * removed sam from buildspec
    
    * echo s3_bucket
    
    * changed package parameters
    
    * removed echo statement
    
    * added globals section
    
    * added sequence path
    
    * reports sequence
    
    * updated filenames
    
    * added state method
    
    * added state function
    
    * refactored iotClient
    
    * refactored logging
    
    * added code to trigger IoT on showsequence
    
    * converted to space-tabs
    
    * converted to keyword argument
    
    * decodes seq as an integer, if not raises exception
    
    * fixed syntax error
    
    * added missing bracket
    
    * added log messages
    
    * fixed another syntax error
    
    * added missing bracket again
    
    * added str() to fix syntax error
    
    * changed to iotClient
    
    * updated error handling
    
    * added off API call
    
    * added quotes
    
    * off api function
    
    * consistency in template naming

---
+++ api.py
@@ -0,0 +1,43 @@
+import json
+import boto3
+import logging
+
+logger = logging.getLogger("api")
+logger.setLevel(logging.DEBUG)
+iotClient = boto3.client('iot-data', region_name='eu-west-1')
+
+def showsequence(event, context):    
+    logger.debug("Received event: " + json.dumps(event, indent=2))
+    try:
+        seq = int(event['pathParameters']['sequence'])
+    except ValueError:
+        raise Exception("Invalid sequence parameter - must be a non-zero integer")
+    logger.debug("Checking integer: " + str(seq))
+    if seq == 0:
+        raise Exception("Invalid sequence parameter - must be non-zero")
+    logger.debug("Valid non-zero integer confirmed")
+        
+    logger.info("Executing showsequence command: " + event['pathParameters']['sequence'])
+    payload = {"state":{"desired":{"status":"TRIGGER","sequence":seq}}}
+    response = iotClient.update_thing_shadow(thingName='ThomasBedroomLEDcontrol',payload=json.dumps(payload))
+    streamingBody = response["payload"]
+    jsonState = json.loads(streamingBody.read())
+    logger.info(jsonState)
+    return {'statusCode': 200, 'body': json.dumps(jsonState), 'headers': {'Content-Type': 'application/json'}}
+
+def off(event, context):    
+    logger.info("Executing off command:")
+    payload = {"state":{"desired":{"status":"TRIGGER","sequence":100}}}
+    response = iotClient.update_thing_shadow(thingName='ThomasBedroomLEDcontrol',payload=json.dumps(payload))
+    streamingBody = response["payload"]
+    jsonState = json.loads(streamingBody.read())
+    logger.info(jsonState)
+    return {'statusCode': 200, 'body': json.dumps(jsonState), 'headers': {'Content-Type': 'application/json'}}
+
+def state(event, context):
+    logger.info("Executing state command (returns state)")
+    response = iotClient.get_thing_shadow(thingName='ThomasBedroomLEDcontrol')
+    streamingBody = response["payload"]
+    jsonState = json.loads(streamingBody.read())
+    logger.debug(jsonState)
+    return {'statusCode': 200, 'body': json.dumps(jsonState), 'headers': {'Content-Type': 'application/json'}}
