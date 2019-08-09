cf3bf4a6b4e920764b5c38fdd99ee7d19f20f147
ddunford <djdunford@gmail.com>
2019-08-09 17:20:39 +0100

    updated identity pool

--- lightsapp/src/config.js
+++ lightsapp/src/config.js
@@ -7,6 +7,5 @@ export default {
     REGION: "eu-west-1",
     USER_POOL_ID: "eu-west-1_L7Ck7fcxb",
     APP_CLIENT_ID: "59vdid1c1ecr8q07e4hhmc4d8m",
-    IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
   }
 };
