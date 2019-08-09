e359580e08f837e9603319d8eabb8f6a52d054f7
ddunford <djdunford@gmail.com>
2019-08-09 16:31:01 +0100

    added AWS configuration

---
+++ lightsapp/src/config.js
@@ -0,0 +1,12 @@
+export default {
+  apiGateway: {
+    REGION: "eu-west-1",
+    URL: "https://api.debsanddarren.com/lights"
+  },
+  cognito: {
+    REGION: "eu-west-1",
+    USER_POOL_ID: "eu-west-1_L7Ck7fcxb",
+    APP_CLIENT_ID: "59vdid1c1ecr8q07e4hhmc4d8m",
+    IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+  }
+};
