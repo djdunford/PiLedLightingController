095d32975253df4e6daf112dbd03e082fd00a778
ddunford <djdunford@gmail.com>
2019-08-09 19:39:10 +0100

    finished logout functionality

--- lightsapp/src/App.js
+++ lightsapp/src/App.js
@@ -4,30 +4,51 @@ import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
 import { LinkContainer } from "react-router-bootstrap";
+import { Auth } from "aws-amplify";
 
 class App extends Component {
 	constructor(props) {
 		  super(props);
 
 		  this.state = {
-		    isAuthenticated: false
-		  };
+				  isAuthenticated: false,
+				  isAuthenticating: true
+				};
+		  
 		}
 
+	async componentDidMount() {
+		  try {
+		    await Auth.currentSession();
+		    this.userHasAuthenticated(true);
+		  }
+		  catch(e) {
+		    if (e !== 'No current user') {
+		      alert(e);
+		    }
+		  }
+
+		  this.setState({ isAuthenticating: false });
+		}
+	
 		userHasAuthenticated = authenticated => {
 		  this.setState({ isAuthenticated: authenticated });
 		}
 
-		handleLogout = event => {
+		handleLogout = async event => {
+			  await Auth.signOut();
+
 			  this.userHasAuthenticated(false);
 			}
 
+		
 		render() {
 		const childProps = {
 				  isAuthenticated: this.state.isAuthenticated,
 				  userHasAuthenticated: this.userHasAuthenticated
 				};
 		  return (
+			!this.state.isAuthenticating &&
 		    <div className="App container">
 		      <Navbar fluid collapseOnSelect>
 		        <Navbar.Header>
