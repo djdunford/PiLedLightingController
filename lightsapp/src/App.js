cb8cb6fc70a55f9c32a6c535067faa2d3f343e44
ddunford <djdunford@gmail.com>
2019-08-09 16:01:08 +0100

    added required import

--- lightsapp/src/App.js
+++ lightsapp/src/App.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
 
 class App extends Component {
 	render() {
