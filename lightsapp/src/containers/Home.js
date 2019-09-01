bff70faf88762ca70d5a1a8f7a2266ab63280e31
Darren Dunford - iPhone <djdunford@gmail.com>
2019-09-01 09:26:05 +0100

    test code to handle red button

--- lightsapp/src/containers/Home.js
+++ lightsapp/src/containers/Home.js
@@ -3,6 +3,12 @@ import { Button } from "react-bootstrap";
 import "./Home.css";
 
 export default class Home extends Component {
+
+handleRed = async event => {
+  event.preventDefault();
+  this.setState({ isLoading: true });
+}
+
   render() {
     return (
       <div className="Home">
@@ -11,7 +17,7 @@ export default class Home extends Component {
           <p>APA102 / SK9822 LED Lighting Controller</p>
         </div>
         <div className="buttons">
-          <Button bsStyle="danger" bsSize="large">RED</Button>
+          <Button bsStyle="danger" bsSize="large" type="button" onClick={this.handleRed}>RED</Button>
           <Button bsStyle="success" bsSize="large">GREEN</Button>
           <Button bsStyle="primary" bsSize="large">BLUE</Button>
         </div>        
