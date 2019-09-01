bc2eac3d68f4d2665049e7c20715fbebb3678d39
ddunford <djdunford@gmail.com>
2019-09-01 10:10:18 +0100

    refactor indentation

--- lightsapp/src/Routes.js
+++ lightsapp/src/Routes.js
@@ -5,10 +5,10 @@ import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import AppliedRoute from "./components/AppliedRoute";
 
-  export default ({ childProps }) =>
-  <Switch>
-    <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <AppliedRoute path="/login" exact component={Login} props={childProps} />
-    { /* Finally, catch all unmatched routes */ }
-    <Route component={NotFound} />
-  </Switch>;
+export default ({ childProps }) =>
+<Switch>
+<AppliedRoute path="/" exact component={Home} props={childProps} />
+<AppliedRoute path="/login" exact component={Login} props={childProps} />
+{ /* Finally, catch all unmatched routes */ }
+<Route component={NotFound} />
+</Switch>;
