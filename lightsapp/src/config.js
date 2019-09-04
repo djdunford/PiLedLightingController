6389b4a04667d0d7d875bcf70d3515e0f8b11f32
djdunford <djdunford@gmail.com>
2019-09-04 17:03:26 +0100

    updated user pool id in config.js

--- lightsapp/src/config.js
+++ lightsapp/src/config.js
@@ -6,6 +6,6 @@ export default {
   cognito: {
     REGION: "eu-west-1",
     USER_POOL_ID: "eu-west-1_L7Ck7fcxb",
-    APP_CLIENT_ID: "59vdid1c1ecr8q07e4hhmc4d8m",
+    APP_CLIENT_ID: "4jdl5geaahich5idjs0c52o200",
   }
 };
