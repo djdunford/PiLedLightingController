0360ef9a0725a7fb0772bb0b455366d78234f429
djdunford <djdunford@gmail.com>
2019-09-09 06:35:24 +0100

    change to AWS_IAM authentication

--- lightsapp/src/containers/Home.js
+++ lightsapp/src/containers/Home.js
@@ -2,16 +2,11 @@ import React, { Component } from "react";
 import { Button } from "react-bootstrap";
 import "./Home.css";
 import { API } from "aws-amplify";
-import { Auth } from "aws-amplify";
 
 export default class Home extends Component {
 
 handleRed = async event => {
   event.preventDefault();
-  this.setState({ disabled: true });
-  const user = await Auth.currentAuthenticatedUser();
-  const token = user.signInUserSession.idToken.jwtToken;
-  const request = { headers: { Authorization: token } };
   API.get("ledlightingcontroller", "/showsequence/1", request)
       .catch(error => {console.log(error.response)});
 };
