ac6316dd564a3f1d617b133938118fd1dcdcee52
djdunford <djdunford@gmail.com>
2019-09-15 17:06:42 +0100

    added cognito identity pool ID to amplify configuration

--- lightsapp/src/config.js
+++ lightsapp/src/config.js
@@ -1,11 +1,12 @@
 export default {
   apiGateway: {
-    REGION: "eu-west-1",
-    URL: "https://api.debsanddarren.com/lights"
+    REGION: "eu-west-1",  // TODO remove hardcoded region
+    URL: "https://api.debsanddarren.com/lights" // TODO hardcoded API URI
   },
   cognito: {
-    REGION: "eu-west-1",
-    USER_POOL_ID: "eu-west-1_L7Ck7fcxb",
-    APP_CLIENT_ID: "4jdl5geaahich5idjs0c52o200",
+    REGION: "eu-west-1",  // TODO remove hardcoded region
+    USER_POOL_ID: "eu-west-1_L7Ck7fcxb",  // TODO remove hardcoded user pool ID
+    APP_CLIENT_ID: "4jdl5geaahich5idjs0c52o200",  // TODO remove hardcoded app client ID
+    IDENTITY_POOL_ID: "eu-west-1:ee092402-95fa-474e-bbfc-340ab7535da8"  // TODO remove hardcoded identity pool ID
   }
 };
