37ac71f4f75e174b0be67fa9053207ff5d556dbf
djdunford <djdunford@gmail.com>
2019-09-15 21:28:11 +0100

    added Auth library to Home.js

--- lightsapp/src/containers/Home.js
+++ lightsapp/src/containers/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Button } from "react-bootstrap";
 import "./Home.css";
 import { API } from "aws-amplify";
+import { Auth } from 'aws-amplify';
 import config from "../config";
 
 export default class Home extends Component {
