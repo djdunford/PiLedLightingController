baacf88cc074e2bb4cdb7a5a10985571a354bf8c
Darren <djdunford+git@gmail.com>
2019-10-19 17:44:34 +0100

    test lights rotates through R, G, B

--- testleds.py
+++ testleds.py
@@ -13,10 +13,28 @@ import time
 ledstrip = apa102.APA102(num_led=146)
 ledstrip.clear_strip()
 
-for i in range(146):
-    ledstrip.set_pixel(i,255,0,0,10)
-ledstrip.show()
-
+# loop forever
 while True:
+
+    for i in range(146):
+        ledstrip.set_pixel(i,255,0,0,10)
+    ledstrip.show()
+
     time.sleep(1);
 
+    for i in range(146):
+        ledstrip.set_pixel(i,0,255,0,10)
+    ledstrip.show()
+
+    time.sleep(1);
+
+    for i in range(146):
+        ledstrip.set_pixel(i,0,0,255,10)
+    ledstrip.show()
+
+    time.sleep(1);
+    
+    ledstrip.clear_strip();
+    time.sleep(1);
+    
+    
